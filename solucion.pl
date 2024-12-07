8ee986e2d0d8a8321e9618a1c6c974d2fdc30c7c
MatiasV10 <63935094+MatiasV10@users.noreply.github.com>
2024-12-07 13:51:22 -0300

    Update solucion.pl

--- solucion.pl
+++ solucion.pl
@@ -1,3 +1,4 @@
+% Solución en Prolog
 viveEnElBarrioDe(alf, almagro).
 viveEnElBarrioDe(juan, almagro).
 viveEnElBarrioDe(nico, almagro).
@@ -11,16 +12,21 @@ viveEnUnDepartamento(nico, 3, 2).
 viveEnUnDepartamento(alf, 3, 1).
 viveEnUnDepartamento(vale, 4, 1).
 viveEnUnLoft(julian, 2000).
+valeLaCasa(juan, 150000).
+valeLaCasa(nico, 80000).
+valeLaCasa(alf, 75000).
+valeLaCasa(julian, 140000).
+valeLaCasa(vale, 95000).
+valeLaCasa(fer, 60000).
 
-viveEnPropiedadesCopadas(Personas):- (viveEnUnaCasa(Personas, MetrosCuadrados), MetrosCuadrados>100)
+viveEnPropiedadesCopadas(Personas):- (viveEnUnaCasa(Personas, MetrosCuadrados), MetrosCuadrados>100).
 viveEnPropiedadesCopadas(Personas):- (viveEnUnDepartamento(Personas, Ambientes, Banios), Ambientes>3).
 viveEnPropiedadesCopadas(Personas):- (viveEnUnDepartamento(Personas, Ambientes, Banios), Banios>1).
 viveEnPropiedadesCopadas(Personas):- (viveEnUnLoft(Personas, Anio), Anio>2015).
 
 barrioCopado(Barrio):- (viveEnUnaCasa(Personas, MetrosCuadrados), MetrosCuadrados>100) , (viveEnUnDepartamento(Personas, Ambientes, Banios), Ambientes>3).
 
-barrioCaro(Barrio):- not (barato(Casa)).
-
-                    barato(Casa):- (viveEnUnLoft(Personas, Anios), Anios<2005).
-                    barato(Casa):- (viveEnUnCasa(Personas, MetrosCuadrados), MetrosCuadrados<90).
-                    barato(Casa):- (viveEnUnDepartamento(Personas, Ambientes, Banios), Ambientes<3).
+barrioCaro(Barrio):-not(barato(Casa)).
+                    barato(Casa):- (viveEnUnLoft(_, Anios), Anios<2005).
+                    barato(Casa):- (viveEnUnaCasa(_, MetrosCuadrados), MetrosCuadrados<90).
+                    barato(Casa):- (viveEnUnDepartamento(_, Ambientes, _), Ambientes<3).
